25977a72c6596f90a322b3a1b46a4f5774a1dc56
Lopdefxxm <qiang5256>
2019-02-12 20:15:45 +0800

    项目框架

---
+++ src/main/java/org/project/upserver/UpServerApplication.java
@@ -0,0 +1,14 @@
+package org.project.upserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpServerApplication.class, args);
+    }
+
+}
+
